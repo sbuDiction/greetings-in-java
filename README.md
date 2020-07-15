8e97eb1a24f7a04c4dbdd1a5a8332ae1f7c69a95
sbuDiction <nsbu68501@gmail.com>
2020-07-16 00:21:17 +0200

    h2 db working

---
+++ README.md
@@ -0,0 +1,15 @@
+# Greetings-in-java
+# Terminal commands.
+* for starting the h2 tcp sever
+    - java -cp h2*.jar org.h2.tools.Server
+    
+    #
+    * For connecting to the h2 tool.
+* java -cp h2-*.jar org.h2.tools.Shell.
+
+| mvn   |      descriptions      |  Type |
+|----------|:-------------:|------:|
+| mvn flyway:migrate |  create db tables | mvn |
+| mvn flyway:info |    shows info db   |   mvn |
+| mvn flyway:clean | drops all db objects |    mvn |
+    
--- src/main/java/net/greet/Greet.java
+++ src/main/java/net/greet/Greet.java
@@ -1,9 +1,25 @@
 package net.greet;
 
+import java.sql.*;
+
 public class Greet {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+    //            final String jdbcURL = "jdbc:h2:file:./target/greetings_db";
+            Class.forName("org.h2.Driver");
+
+            // connect to the database
+            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://127.0.1.1:9092/~/IdeaProjects/greet-in-java/target/greetings_app_db", "sa", "");
+            PreparedStatement results = conn.prepareStatement("SELECT * FROM USERS WHERE NAME = 'sbu'");
+            ResultSet list = results.executeQuery();
+
+            System.out.println(conn.getMetaData());
+            System.out.println(list.getString(2));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
