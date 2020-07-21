ccdca5acb437298abc07e073b0696c06b547d29d
sbuDiction <nsbu68501@gmail.com>
2020-07-21 02:28:09 +0200

    working on the format for greeting

---
+++ src/main/java/net/greet/commands/Commands.java
@@ -0,0 +1,10 @@
+package net.greet.commands;
+
+public enum Commands {
+    greet,
+    greeted,
+    counter,
+    clear,
+    exit,
+    help
+}
--- src/main/java/net/greet/tools/StringFormat.java
+++
@@ -1,9 +0,0 @@
-package net.greet.tools;
-
-public class StringFormat {
-
-
-    public void checkFormat() {
-
-    }
-}
